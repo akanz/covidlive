2146405cbcdf1412a32d33f316f6ec34ba8a12c7
Manakanz <samuelakande2014@yahoo.com>
2020-05-18 13:45:22 +0100

    fixes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging,request
 import requests, json
 from datetime import date
+from config import secret_key
 
 
 app = Flask(__name__)
@@ -27,34 +28,40 @@ def result():
         name = name.lower()
         slug = name.replace(' ','-')
 
-        res1 = requests.get('https://api.covid19api.com/country/{}?from=2020-03-01T00:00:00Z&to={}T00:00:00Z'.format(slug,today))
+        #res1 = requests.get('https://api.covid19api.com/country/{}?from=2020-03-01T00:00:00Z&to={}T00:00:00Z'.format(slug,today))
 
         try:
-            if resp.status_code == 200 or res1.status_code == 200:
+            if resp.status_code == 200:
                 summ = [resp.json()]
-                total = res1.json()
+                #total = res1.json()
                 for j in summ:
                     data = j['Countries']
 
                 for i in data:
-                    country = i['Country'].lower()
+                    country = i['Slug']
+                    country = country.replace('-',' ')
                     if country == name:
                         new_data = i
                     else:
                         continue
-                    return render_template('result.html',new_data=new_data,total=total)
+                    return render_template('result.html',new_data=new_data)
             else:
+                app.logger.error('Error {} occurred'.format(str(resp.status_code)))
+                flash('something went wrong, try again.', 'danger')
                 return redirect(url_for('error'))
         except:
+            app.logger.error('Invalid response')
+            flash('Oops! There must be a connection error', 'warning')
             return redirect(url_for('home'))
 
     return result()
 
 @app.route('/error')
 def error():
-    return render_template('404-page.html')
+    return redirect(url_for('home'))
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = secret_key
+    app.run(debug=True)
