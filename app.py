41af5370c099dc6fcefe74c22e201ff122874c51
Manakanz <samuelakande2014@yahoo.com>
2020-06-06 07:07:56 +0100

    add flashing messages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging,request
 import requests, json
 from datetime import date
-#from config.config_dev import secret_key
+from config.config_dev import secret_key
 
 
 app = Flask(__name__)
@@ -20,39 +20,39 @@ def home():
             new_data = data
         return render_template('index.html',new_data=new_data )
 
-    else:
-        if request.method == 'POST':
-            name = request.form['text']
-            name = name.lower()
-            slug = name.replace(' ', '-')
-            try:
-                if res.status_code == 200:
-                    total = [res.json()]
-                    # total = res1.json()
-                    for j in total:
-                        data = j['Countries']
-
-                    for i in data:
-                        country = i['Slug']
-                        country = country.replace('-', ' ')
-                        if country == name:
-                            new_data = i
-                        else:
-                            continue
-                        return render_template('result.html', new_data=new_data)
-                else:
-                    app.logger.error('Error {} occurred'.format(str(res.status_code)))
-                    # flash('something went wrong, try again.', 'danger')
-                    return redirect(url_for('error'))
-            except:
-                app.logger.error('Invalid response')
-                # flash('Oops! There must be a connection error', 'warning')
-                return redirect(url_for('error'))
+    # else:
+    #     if request.method == 'POST':
+    #         name = request.form['text']
+    #         name = name.lower()
+    #         slug = name.replace(' ', '-')
+    #         try:
+    #             if res.status_code == 200:
+    #                 total = [res.json()]
+    #                 # total = res1.json()
+    #                 for j in total:
+    #                     data = j['Countries']
+    #
+    #                 for i in data:
+    #                     country = i['Slug']
+    #                     country = country.replace('-', ' ')
+    #                     if country == name:
+    #                         new_data = i
+    #                     else:
+    #                         continue
+    #                     return render_template('result.html', new_data=new_data)
+    #             else:
+    #                 app.logger.error('Error {} occurred'.format(str(res.status_code)))
+    #                 # flash('something went wrong, try again.', 'danger')
+    #                 return redirect(url_for('error'))
+    #         except:
+    #             app.logger.error('Invalid response')
+    #             # flash('Oops! There must be a connection error', 'warning')
+    #             return redirect(url_for('error'))
 
     return home()
 
 
-@app.route('/result/', methods=['GET','POST'])
+@app.route('/result', methods=['GET','POST'])
 def result():
     resp = requests.get('https://api.covid19api.com/summary')
     if request.method == 'POST':
@@ -77,21 +77,21 @@ def result():
                     return render_template('result.html',new_data=new_data)
             else:
                 app.logger.error('Error {} occurred'.format(str(resp.status_code)))
-                #flash('something went wrong, try again.', 'danger')
+                flash('something went wrong, try again.', 'danger')
                 return redirect(url_for('error'))
         except:
             app.logger.error('Invalid response')
-            #flash('Oops! There must be a connection error', 'warning')
+
             return redirect(url_for('error'))
 
-    return redirect('result')
+    return redirect('error')
 
 @app.route('/error')
 def error():
+    flash('something went wrong, try again.', 'danger')
     return redirect(url_for('home'))
 
 
-
 if __name__ == '__main__':
-
+    app.secret_key = secret_key
     app.run(debug=True)
